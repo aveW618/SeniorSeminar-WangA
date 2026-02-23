1ccb33c2de8be5cd1a09482f36ecba29ea399a2d
aveW618 <146094921+aveW618@users.noreply.github.com>
2026-02-23 00:28:35 -0500

    Add files via upload
    changing the counter to loop through the data and count how many times each session appears

--- Counter.java
+++ Counter.java
@@ -3,50 +3,32 @@ import java.util.*;
 
 public class Counter {
 	public static void main(String[] args) throws IOException {
-
+		
+		//importing data from the files
         String filename = "Senior Seminar Data.csv";
         File myFile = new File(filename);
         Scanner scan = new Scanner(myFile);
         
-	private int session1 = 0;
-	private int session2 = 0;
-	private int session3 = 0;
-	private int session4 = 0;
-	private int session5 = 0;
-	private int session6 = 0;
-	private int session7 = 0;
-	private int session8 = 0;
-	private int session9 = 0;
-	private int session10 = 0;
-	private int session11 = 0;
-	private int session12 = 0;
-	private int session13 = 0;
-	private int session14 = 0;
-	private int session15 = 0;
-	private int session16 = 0;
-	
-	String filename = "Senior Seminar Data.csv";
-	File myFile = new File(filename);
-	Scanner scan = new Scanner(myFile);
-	int i = 0;
-		
-	if (scan.hasNextLine()) {
-		scan.nextLine();
-	}
-	while (scan.hasNextLine()) {
-		String line = scan.nextLine();
-		String[] data = line.split(",");
-		ArrayList<String> choices = new ArrayList<String>();
-		student.add(data[10]);
-		student.add(data[11]);
-		student.add(data[12]);
-		student.add(data[13]);
-		student.add(data[14]);
-	}
-	
-	for (int row = 0; row < choices.size; row++)
-	{
-		for (int col = 0; col < choices[0].size; col++)
+        //session number array
+        int[] counts = new int[17];
+        
+		if (scan.hasNextLine()) {
+			scan.nextLine();
+		}
+		while (scan.hasNextLine()) {
+			String line = scan.nextLine();
+			String[] data = line.split(",");
+			//reads the 5 choices from the data set
+			for (int choice = 10; choice < 15; choice++) {
+				//takes the chosen session number from the data
+				int sid = Integer.parseInt(data[c]);
+				//increases the chosen session number from above's popularity by 1 for the counts array
+				counts[sid]++
+				
+				for (int col = 0; col < choices[0].size; col++)
 		{
+	}
+	
+
 			
 		
