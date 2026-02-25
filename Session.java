9b6a5f285b1dee321492c3a66076290b96726099
aveW618 <146094921+aveW618@users.noreply.github.com>
2026-02-24 22:39:36 -0500

    Add files via upload
    in the process of loading session files like how I did the student and instructor ones, commenting

--- Session.java
+++ Session.java
@@ -8,10 +8,10 @@ public class Session {
 	private String instructor;
 	
 	//constructor for a session
-	public Session(int sessionID, String sessionName, String instructor) {
-		this.sessionID = sessionID;
-		this.sessionName = sessionName;
-		this.instructor = instructor;
+	public Session(int sid, String sn, String instr) {
+		this.sessionID = sid;
+		this.sessionName = sn;
+		this.instructor = instr;
 	}
 	
 	//getters
@@ -29,12 +29,26 @@ public class Session {
 	public String toString() {
 		return("Session ID: " + sessionID + "\n" + "Session Name: " + sessionName + "\n" + "Instructor: " + instructor);
 	}
-}
-	
-	
-	
-	
-	
-	
-	
-	
+
+//method to load sessions
+public static ArrayList<Session> loadSessions(String filename) throws IOException {
+		// creates an array list to store Session objects
+		ArrayList<Session> sessions = new ArrayList<>();
+		
+		// reads in data from the imported file
+		File myFile = new File(filename);
+		Scanner scan = new Scanner(myFile);
+		
+		// skips a header row if it is in the data
+		if (scan.hasNextLine()) {
+			scan.nextLine();
+		}
+		
+		// reads through each row and creates session objects
+		while (scan.hasNextLine()) {
+			String line = scan.nextLine();
+			String[] data = line.split(",");
+			
+			//stores the specific data for instructor name and their session ID
+			String instructorName = data[17];
+			int sid = Integer.parseInt(data[18]);
