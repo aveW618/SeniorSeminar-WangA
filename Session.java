5b15163dc67cb934eebdba23b46cba300fce9108
aveW618 <146094921+aveW618@users.noreply.github.com>
2026-02-24 22:48:26 -0500

    Add files via upload
    finished loading sessions code based off of my code for instructors and students

--- Session.java
+++ Session.java
@@ -4,13 +4,11 @@ import java.util.*;
 public class Session {
 	//instance variables for session info
 	private int sessionID;
-	private String sessionName;
 	private String instructor;
 	
 	//constructor for a session
-	public Session(int sid, String sn, String instr) {
+	public Session(int sid, String instr) {
 		this.sessionID = sid;
-		this.sessionName = sn;
 		this.instructor = instr;
 	}
 	
@@ -18,20 +16,17 @@ public class Session {
 	public int getSessionID() {
 		return sessionID;
 	}
-	public String getSessionName() {
-		return sessionName;
-	}
 	public String getInstructor() {
 		return instructor;
 	}
 	
 	//toString method
 	public String toString() {
-		return("Session ID: " + sessionID + "\n" + "Session Name: " + sessionName + "\n" + "Instructor: " + instructor);
+		return("Session ID: " + sessionID + "\n" + "Instructor: " + instructor);
 	}
 
-//method to load sessions
-public static ArrayList<Session> loadSessions(String filename) throws IOException {
+	//method to load sessions
+	public static ArrayList<Session> loadSessions(String filename) throws IOException {
 		// creates an array list to store Session objects
 		ArrayList<Session> sessions = new ArrayList<>();
 		
@@ -50,5 +45,33 @@ public static ArrayList<Session> loadSessions(String filename) throws IOExceptio
 			String[] data = line.split(",");
 			
 			//stores the specific data for instructor name and their session ID
-			String instructorName = data[17];
+			//trim to 
+			String instr = data[17].trim();
 			int sid = Integer.parseInt(data[18]);
+			
+			//method of array lists to create a new Session object to add to the arraylist sessions
+			sessions.add(new Session(sid, instr));
+		}
+		
+		//ends the scanning of the data
+		scan.close();
+		
+		return sessions;
+	}
+	
+	// main method to test if the loadSessions method works
+	public static void main(String[] args) throws IOException {
+		// calls the loadSessions method and inputs senior seminar data
+		ArrayList<Session> sessions = loadSessions("Senior Seminar Data.csv");
+
+		// prints out the Session objects to make sure the program runs as intended: loads sessions
+		for (int i = 0; i < sessions.size(); i++) {
+			System.out.println(sessions.get(i));
+		}
+
+		// prints out the number of sessions successfully loaded
+		System.out.println("Total Sessions loaded: " + sessions.size());
+	}
+}
+	
+		
