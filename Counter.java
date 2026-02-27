20d286787301327c18b7688ed3471953bd293b39
aveW618 <146094921+aveW618@users.noreply.github.com>
2026-02-27 09:33:31 -0500

    Add files via upload
    commenting and printing resuls to check if the code successfully performed the desires action of tracking the choices of students

--- Counter.java
+++ Counter.java
@@ -12,22 +12,39 @@ public class Counter {
         //session number array
         int[] counts = new int[17];
         
+        //checks if there is code to read
 		if (scan.hasNextLine()) {
 			scan.nextLine();
 		}
+		
+		//reads the code and stores it so that I can keep track of popularity
 		while (scan.hasNextLine()) {
 			String line = scan.nextLine();
 			String[] data = line.split(",");
 			//reads the 5 choices from the data set
 			for (int choice = 10; choice < 15; choice++) {
 				//takes the chosen session number from the data
-				int sid = Integer.parseInt(data[c]);
+				int sid = Integer.parseInt(data[choice]);
 				//increases the chosen session number from above's popularity by 1 for the counts array
-				counts[sid]++
-				
-				for (int col = 0; col < choices[0].size; col++)
-		{
+				if (sid >= 1 && sid <= 16) {
+					counts[sid]++;
+				}
+			}
+		}
+		scan.close();
+		
+		//sid is session id so it prints out the results from the previous loops)
+		for (int sid = 1; sid <= 16; sid++) {
+			System.out.println("Session " + sid + ": " + counts[sid]);
+		}
+		
+		//helps me check if the class works and fills up different sessions, stopping when the sessions are filled
+		if (counts[sid] > 16) {
+			System.out.println("Session " sid + " full");
+		}
 	}
+}
+			
 	
 
 			
