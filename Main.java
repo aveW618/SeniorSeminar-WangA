ad9d34c83ec80c46bafc9a8e23e54d1b8a97d66d
aveW618 <146094921+aveW618@users.noreply.github.com>
2026-03-12 09:32:03 -0400

    Add files via upload
    Worked on the next processes for loading the actual schedules themselves through popularity by improving my Counter class which keeps track of popularity. I wanted to create the 2nd running of a session if it so popular that the 1st running is filled. I still need to work on syntax and changing the errors but my logic is there. I also started by working on creating my 5 x 5 timetable for my array list and was going to loop through the rows and the session instructor information to make sure that an instructor isn't teaching two sessions at the same time, but Mr. Twyford recommended that I first work on laying down a schedule as a basis and later consider these details.

--- Main.java
+++ Main.java
@@ -8,9 +8,21 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//collects all the student choices
-		ArrayList<StudentChoices> totalChoices = new ArrayList<>();
-		//collects all the individual session IDs
-		ArrayList<Integer> sessionIds = new ArrayList<>();
+		ArrayList<StudentChoices> totalChoices = new ArrayList<<5><5>>();
+		
+		//loading the sessions into an arrayList that is structured like the timetable
+			//5 times and 5 sessions at one time
+		totalchoices.loadSessions;
+		
+		//ignore this section for now, focus on first creating a schedule 
+			//and then later on about making sure one instructor does not have two classes at the same time
+			
+		//loops through rows of the array list
+		//for (int i = 0; i < 5; i++) {
+			//loops through all the sesions of the row of the array list that has been reached in the first for loop
+			//for (int i = 0; i < sessions.size; i++) {
+				//creates an if loop to compare the sessions in the row of the array list that is being accessed
+				//if (sessions.get(0) 
 	
 		//loop through every student
 		for (int i = 0; i < totalChoices.size(); i++) {
@@ -21,4 +33,5 @@ public class Main {
 			int[] rankedChoices = {sc.getChoice1(), sc.getChoice2(), sc.getChoice3(), sc.getChoice4(), sc.getChoice5()};
 		
 			for (int t = 0; t < 5; t++) {
+				
 				
